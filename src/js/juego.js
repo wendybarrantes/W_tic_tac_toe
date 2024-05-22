7fdb5c59a3731a55d4c4f6d00bd8728a40929531
wendybarrantes <wenbarrantes06@gmail.com>
2024-05-22 15:48:50 -0600

    logica del juego

--- src/js/juego.js
+++ src/js/juego.js
@@ -1,3 +1,4 @@
+    //referenciar los id que estan en html con variables del js.
     let casilla_1 = document.getElementById("casilla_1")
     let casilla_2 = document.getElementById("casilla_2")
     let casilla_3 = document.getElementById("casilla_3")
@@ -7,14 +8,21 @@
     let casilla_7 = document.getElementById("casilla_7")
     let casilla_8 = document.getElementById("casilla_8")
     let casilla_9 = document.getElementById("casilla_9")
-
+    
+    //creo un arreglo y guardo todas las casillas
     let contenedores = [casilla_1, casilla_2, casilla_3,casilla_4,casilla_5,casilla_6,casilla_7,
         casilla_8, casilla_9]
 
-// casilla_1.innerHTML = "x"
+     /*
+    forEach : para cada.
 
-//forEach : para cada.
-function movimientoCasillas () { 
+    creo  la funcion para manipular el tablero.
+    verifico todas las posiciones para ver si estan vacias y si lo estan uso el inner.html para
+    cambiar el valor, luego del turno del jugador ejecuto el movimiento aleatorio y luego 
+     se ejecuta la funcion para verificar el ganador .
+    utilizo la propiedad forEach para asignar el evento a cada casilla.
+     */
+    function movimientoCasillas () { 
     contenedores.forEach((casilla)=> casilla.addEventListener("click", ()=>{
         if (casilla.innerHTML == "") {
             casilla.innerHTML = "✖️"
@@ -24,10 +32,14 @@ function movimientoCasillas () {
             ganador()
 
         }
-
-
     }))
 }
+    /*creo la función para manipular el movimiento de la pc.
+    utilizo el metodo filter que me devuelve un arreglo nuevo según la condición
+     (visto en clase) para guardar en una variable el nuevo arreglo vacío.
+     utilizo la función propia de js math random para que me de un numero aleatorio.
+     Este número aleatorio va a ser la posición en la que se va a colocar el circulo. 
+     */
 function movimientoPc() {
     let contenedoresVacios = contenedores.filter((casilla) => casilla.innerHTML == "")
     let aleatorio =  Math.floor(Math.random() * contenedoresVacios.length);
@@ -38,7 +50,12 @@ function movimientoPc() {
 }
 movimientoCasillas()
 
-function ganador() {
+    /*Creo la función ganador para que identifique e indique cuando el jugador gana.
+    creo una variable indicativa que contiene arreglos con las posibles posiciones ganadoras.
+    utilizo la función For of para recorrer cada pequeño arreglo verificando en cada iteración
+    si hay un ganador. 
+    */
+    function ganador() {
 
     let posicionesGanadoras = [
         [0,1,2],[3,4,5],[6,7,8], //filas
@@ -48,7 +65,7 @@ function ganador() {
     for (const iterar of posicionesGanadoras) {
         [pos1,pos2,pos3] = iterar
     if (contenedores[pos1].innerHTML && contenedores[pos1].innerHTML == contenedores[pos2].innerHTML && contenedores[pos1].innerHTML == contenedores[pos3].innerHTML ) {
-        console.log("Entre");           
+        alert("FELICIDADES, GANASTE!");           
         return true
             
         }
